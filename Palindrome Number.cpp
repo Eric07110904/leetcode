15fb61c84b9321bf23d63ea03164659888602597
azsx26735546 <azsx26735546@gmail.com>
2019-07-20 16:06:49 +0800

    add

--- Palindrome Number.cpp
+++ Palindrome Number.cpp
@@ -7,7 +7,7 @@ public:
         for(int i=x;i!=0;i/=10){
             if (sum> INT_MAX/10 || sum < INT_MIN/10)
                 return false; 
-            sum=i%10+10*sum;
+            sum=i%10+10*sum;//這個數字反轉方法很好用 不用取得字串長度就能做reverse了
         }
         return x==sum;
         
